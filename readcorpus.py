53edc318595dc45d28324b09fa5cc3b61d653dfe
mcaswell <Marie@10-196-39-107.wireless.oregonstate.edu>
2015-02-24 15:29:48 -0800

    added michael's changes

--- readcorpus.py
+++ readcorpus.py
@@ -2,6 +2,8 @@
 
 import json, sys, getopt, os
 
+REC = {}
+
 def usage():
   print("Usage: %s --file=[filename]" % sys.argv[0])
   sys.exit()
@@ -25,9 +27,27 @@ def main(argv):
   urldata = json.load(corpus, encoding="latin1")
 
   for record in urldata:
- 
+    rec_ip = record["ips"].get("ip")
+    score = 0
     # Do something with the URL record data...
-    print record["domain_age_days"]
+    REC[rec_ip] = 0
+    if record["domain_age_days"] < 188:
+      score = score + 1
+    if record["host_len"] > 10:
+      score = score + 1
+    if record["default_port"] != 80 or record["default_port"] != 443:
+      score = score + 1
+    if record["tld"] not in ("com","net","org","edu"):
+      score = score + 1
+    if record["alexa_rank"] > 1000000 or record["alexa_rank"] == NULL:
+      score = score + 1
+    if record["num_domain_tokens"] > 3:
+      score = score + 1
+
+    REC[rec_ip] = score
+
+  for r,s in REC:
+    print(r,s)
 
   corpus.close()
 
