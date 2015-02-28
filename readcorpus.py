f43d9a9dc4ff721cb903bf879d4581783a446b26
mcaswell <Marie@Maries-MacBook-Pro.local>
2015-02-27 21:04:49 -0800

    minor fixes, added count, increased value of .exe
    Conflicts:
    	readcorpus.py

--- readcorpus.py
+++ readcorpus.py
@@ -12,6 +12,7 @@
 import json, sys, getopt, os
 
 REC = {} #empty dictionary structure to store url:score
+COUNT = {} #added count to see point spread among URLs
 
 # Note: I changed the REC{} format to store the URL 
 # instead of the IP address since lab2.pdf says 
@@ -39,17 +40,23 @@ def main(argv):
  
   corpus = open(file)
   urldata = json.load(corpus, encoding="latin1")
+  print len(urldata)
+  domain_age_total = 0
 
   for record in urldata:
     rec_url = record['url']
     score = 0
+
     
     # Do something with the URL record data...
     
     REC[rec_url] = 0
-    
+
     if record["domain_age_days"] < 188:
       score = score + 1
+    # I made this rule by taking the average domain age of the data set.  I noticed that not a single domain above this average was malicious.
+    if record["domain_age_days"] > 1300:
+      score = score - 1
     if record["host_len"] > 10:
       score = score + 1
     if record["default_port"] != 80 or record["default_port"] != 443:
@@ -60,13 +67,52 @@ def main(argv):
       score = score + 1
     if record["num_domain_tokens"] > 3:
       score = score + 1
+    # It stands to reason that links with an executable file have a beter chance of being malicious.  
+
+    if record["file_extension"] == ("exe"):
+      score = score + 5
+
+    if "mxhosts" in record:
+      if record["mxhosts"] != None:
+        i = record["mxhosts"]
+        if i[0] == None:
+          print "no mx host"
+          score = score + 1
+        else:
+          print "mx host exists"
+          mx_loc = i[0]
+
+    if  record["ips"] != None:
+      i = record["ips"]
+      if i[0] == None:
+        print "None\n"
+        score = score + 1
+      else:
+        print "exists"
+        dom_loc = i[0]
+
+    if mx_loc == dom_loc:
+      score = score - 1
+    else:
+      score = score + 1
 
     REC[rec_url] = score
 
+  output = open('output.txt','w') #delete any old data from the output file
+  output.truncate(0)
+  output.close()
+
   for r,s in REC.iteritems():
-    print(r,s)
+    with open('output.txt', 'a') as output:     #open output file in append mode
+      print(r,s)
+      output.write('%s, %s\n' % (r, str(s))) #write new data to file
+
+  for r,s in REC.iteritems():
+    COUNT[s] = COUNT.get(s,0)+1
+  print COUNT
 
   corpus.close()
+  output.close()
 
 if __name__ == "__main__":
   main(sys.argv[1:])
